8e831aa55b3af9541cda7936ada65421a1ff5632
kavindu ranasinghe <kavinduranasingha57@gmail.com>
2024-05-16 14:37:49 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -15,8 +15,26 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 4221),reuse_port=True)
-    server_socket.accept() # wait for client
+    server_socket = socket.create_server(("localhost", 4221))
+    print("server is running on port 4221")
+
+    try:
+        while True:
+            print("waiting for connection")
+            client_socket, addr = server_socket.accept() # wait for client
+
+            print(f"connection from {addr} has been eshtablished.")
+
+            # handle the client request
+            handle_request(client_socket)
+
+            # close the connection to the client
+            client_socket.close()
+    except KeyboardInterrupt:
+        print("\nServer is shutting down.")
+    finally:
+        server_socket.close()
+        print("server has been shut down.")
 
 
 if __name__ == "__main__":
