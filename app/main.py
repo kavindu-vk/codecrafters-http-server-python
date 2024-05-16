7e76005cd17b9b2fc22091451a81a14eb581da8e
kavindu ranasinghe <kavinduranasingha57@gmail.com>
2024-05-16 14:36:23 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 # Uncomment this to pass the first stage
 import socket
 
+def handle_request(client_socket):
+    # Read data from the client
+    client_socket.recv(1024) #Reading a bit of data
+
+    # send a 200 OK response
+    response = "HTTP/1.1 200 OK\r\n\r\n"
+    client_socket.send(response.encode())
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,7 +15,7 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 4221))
+    server_socket = socket.create_server(("localhost", 4221),reuse_port=True)
     server_socket.accept() # wait for client
 
 
