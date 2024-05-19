36321a5ae82d1845a3c8cbc3b956560da8f4b836
kavindu ranasinghe <kavinduranasingha57@gmail.com>
2024-05-19 14:00:22 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -86,7 +86,7 @@ def handle_request(client_socket, directory):
 
 def main():
     if len(sys.argv) != 3 or sys.argv[1] != '--directory':
-        print("Usage: ./your_server.py --directory <directory>")
+        print("Usage: ./your_server.sh --directory <directory>")
         sys.exit(1)
 
     directory = sys.argv[2]
@@ -106,10 +106,6 @@ def main():
     print("server is running on port 4221")
 
     try:
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.bind(("localhost", 4221))
-        server_socket.listen(5)
-        print("Server is running on port 4221")
         while True:
             print("waiting for connection")
             client_socket, addr = server_socket.accept() # wait for client
